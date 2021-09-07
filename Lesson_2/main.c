27d8902bdd1c455cb7eb0fbc660b95b6126f86cb
Mesbha <plabon321@yahoo.com>
2021-09-07 02:12:45 +0200

    change flow of control

---
+++ Lesson_2/main.c
@@ -0,0 +1,17 @@
+
+int main()
+{
+  
+  int counter=0;
+  while(counter < 21){
+    ++counter;
+    if((counter & 1) !=0){
+      /*do something when the counter is odd*/
+    }
+    else{
+      /*do something when counter is even*/
+    }
+    
+  
+  return 0;
+}
